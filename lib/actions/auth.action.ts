162ed6b29253ef0dbcc487d528430b2c868ae42e
adityaJadhav77 <aditech2025@gmail.com>
2025-08-15 01:18:52 +0530

    auth-storage

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,121 @@
+"use server";
+
+import { auth, db } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7;
+
+export async function signUp(params: SignUpParams) {
+  const { uid, email, name } = params;
+  try {
+    const userRecord = await db.collection("users").doc(uid).get();
+
+    if (userRecord.exists) {
+      return {
+        success: false,
+        message: "User already exists",
+      };
+    }
+
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+    });
+
+    return {
+      success: true,
+      message: "Account created Please sign in.",
+    };
+  } catch (e: any) {
+    console.error("Error Creating a user", e);
+
+    if (e.code === "auth/email-already-exists") {
+      return {
+        success: false,
+        message: "Email already exists",
+      };
+    }
+
+    return {
+      success: false,
+      message: "Failed to create an account",
+    };
+  }
+}
+
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "User does not exist",
+      };
+    }
+
+    await setSessionCookie(idToken);
+    return {
+      success: true,
+      message: "Logged in successfully",
+    };
+  } catch (e) {
+    console.error(e);
+
+    return {
+      success: false,
+      message: "Failed to log in to an account",
+    };
+  }
+}
+
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK * 1000,
+  });
+
+  cookieStore.set("session", sessionCookie, {
+    maxAge: ONE_WEEK,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+
+  const sessionCookie = cookieStore.get("session")?.value;
+
+  if (!sessionCookie) return null;
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+
+    if (!userRecord.exists) return null;
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.log(error);
+
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+
+  return !!user;
+}
